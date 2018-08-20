551711a958ddbce3b46f6cfacf994e2b41500448
emelone16 <emelone@optonline.net>
2018-08-20 17:50:48 -0400

    Backend Support for saving Days

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require("path");
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 4000;
@@ -10,6 +11,10 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/api/recipes', function(req, res) {
     try {
@@ -22,5 +27,27 @@ app.get('/api/recipes', function(req, res) {
         res.send("ERROR: " + err);
     }
 });
+
+app.get('/api/days', function(req, res) {
+    try {
+        const filePath = path.join(__dirname, 'test-days.json');
+        const file = fs.readFileSync(filePath);
+        const daysJson = JSON.parse(file);
+        res.json(daysJson.days);
+    } catch (err) {
+        res.send("ERROR: " + err);
+    }
+})
+
+app.post('/api/days', function(req, res) {
+    try {
+        const filePath = path.join(__dirname, 'test-days.json');
+        fs.writeFile(filePath, JSON.stringify({days: req.body}), err => {if (err) { throw err }});
+
+        res.send("Success");
+    } catch (err) {
+        res.send("ERROR: " + err);
+    }
+})
 
 app.listen(PORT, () => console.log("App started on port " + PORT));
